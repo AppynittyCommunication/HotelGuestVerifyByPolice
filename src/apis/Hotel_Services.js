8d27e57f6e2f1dfbec3e7a97ea8baa04954f89ab
umeshl@appynitty.com <umeshl@appynitty.com>
2023-08-31 16:13:34 +0530

    done from side

--- src/apis/Hotel_Services.js
+++ src/apis/Hotel_Services.js
@@ -1,9 +1,15 @@
 import axios from "axios";
 
+
 export const GetStateRequest = async()=>{
     const res = await axios({
        method:'get',
        url:'https://hotelapi.ictsbm.com/api/SelectList/GetStates' ,
+       headers: {
+      
+        'Content-Type': 'application/json',
+    }
     });
     return res
+  
 }
