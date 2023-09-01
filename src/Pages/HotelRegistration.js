9fb8cc6302b7a8ae5b56ebb6a58e2be4959131f3
rohittembhurne2194 <tembhurnerohit2194@gmail.com>
2023-09-01 15:39:33 +0530

    Changes Done By Rohit

--- src/Pages/HotelRegistration.js
+++ src/Pages/HotelRegistration.js
@@ -1,18 +1,30 @@
 import React, { useEffect, useState, Component, } from 'react';
 import { Link } from 'react-router-dom';
 import Select, { components } from 'react-select';
-import { getStateListRequest, getDistrictListRequest, getCityListRequest,getPoliceStationListRequest } from '../apis/Hotel_Services'
+import { getStateListRequest, getDistrictListRequest, getCityListRequest,getPoliceStationListRequest, submitHotelRegistration } from '../apis/Hotel_Services'
 import axios from 'axios';
+import { Empty } from 'antd';
 
 export const HotelRegistration = () => {
+  const [fName, setFName] = useState('');
+  const [lName, setLName] = useState('');
+  const [mNumber, setMNumber] = useState('');
+  const [eMail, setEMail] = useState('');
+  const [hName, setHName] = useState('');
+  const [hRegNo, setHRegNo] = useState('');
+  const [hAddress, setHAddress] = useState('');
+  const [hpinCode, setHPinCode] = useState('');
+  const [hLat, setHLat] = useState('');
+  const [hLong, setHLong] = useState('');
+  const [hUsername, setHUsername] = useState('');
   const [stateData, setStateData] = useState([]);
   const [distData, setDistData] = useState([]);
   const [cityData, setCityData] = useState([]);
   const [psData, setPsData] = useState([]);
-  const [selectedStateOption, setSelectedStateOption] = useState(null);
-  const [selectedDistOption, setSelectedDistOption] = useState(null);
-  const [selectedCityOption, setSelectedCityOption] = useState(null);
-  const [selectedPsOption, setSelectedPsOption] = useState(null);
+  const [selectedStateOption, setSelectedStateOption] = useState('');
+  const [selectedDistOption, setSelectedDistOption] = useState('');
+  const [selectedCityOption, setSelectedCityOption] = useState('');
+  const [selectedPsOption, setSelectedPsOption] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
 
@@ -62,6 +74,16 @@ export const HotelRegistration = () => {
       console.log(error);
     }
   }
+
+  const submitHotelReg = async (fName,lName,mNumber,eMail,hName,hRegNo,hAddress,hpinCode,hLat,hLong,hUsername,stateID, distID,cityData,psData) => {
+    try{
+      const res = await submitHotelRegistration(fName,lName,mNumber,eMail,hName,hRegNo,hAddress,hpinCode,hLat,hLong,hUsername,stateID, distID,cityData,psData); // Assuming GetStateRequest returns a promise
+      console.log(res.data)
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
   const stateListOption = stateData.map(function (data) {
     return { value: data.stateId, label: data.stateName }
   })
@@ -102,6 +124,15 @@ export const HotelRegistration = () => {
     setSelectedPsOption(selectedPsOption);
     
   }
+
+  const handleHotelRegSubmit = async(e) =>{
+    e.preventDefault();
+  
+    submitHotelReg(fName,lName,mNumber,eMail,hName,hRegNo,hAddress,hpinCode,hLat,hLong,hUsername,selectedStateOption.value,selectedDistOption.value,selectedCityOption.value,selectedPsOption.value);
+  }
+
+  
+
   return (
     <div style={{ background: '#ebebeb', paddingTop: '1px', height: '100%' }}>
       <div className='px-6 nav-bg' style={{ width: '100%', height: '60px' }}>
@@ -114,7 +145,7 @@ export const HotelRegistration = () => {
       </div>
       <div className='px-6 pt-3'>
         <h5 style={{ background: '#efefef', marginBottom: '0px', width: '30%', padding: '10px', backgroundColor: '#7cb0f5', color: '#fff', borderTopLeftRadius: '10px', borderTopRightRadius: '10px', marginLeft: '20px' }}>Hotel Registration Form</h5>
-        <form className='registrationform_Hotel'>
+        <form className='registrationform_Hotel' onSubmit={handleHotelRegSubmit} method='POST'>
 
           <div className="mb-3 d-flex">
 
@@ -122,16 +153,26 @@ export const HotelRegistration = () => {
               type="text"
               className="form-control"
               placeholder="First name" style={{ marginRight: '80px' }}
+              value={fName}
+              onChange={(e) => setFName(e.target.value)}
             />
-            <input type="text" className="form-control" placeholder="Last name" />
+            <input type="text" className="form-control" 
+            placeholder="Last name"
+            value={lName}
+            onChange={(e) => setLName(e.target.value)} />
           </div>
           <div className="mb-3 d-flex" >
             <input
               type="text"
               className="form-control"
               placeholder="Mobile Number" style={{ marginRight: '80px' }}
+              value={mNumber}
+              onChange={(e) => setMNumber(e.target.value)}
             />
-            <input type="text" className="form-control" placeholder="Email ID" />
+            <input type="email" className="form-control"
+             placeholder="Email ID"
+             value={eMail}
+             onChange={(e) => setEMail(e.target.value)} />
 
           </div>
 
@@ -139,16 +180,25 @@ export const HotelRegistration = () => {
             <input
               type="text"
               className="form-control"
-              placeholder="Hotel Name" style={{ marginRight: '80px' }}
+              placeholder="Hotel Name"
+              style={{ marginRight: '80px' }}
+              value={hName}
+              onChange={(e) => setHName(e.target.value)}
             />
-            <input type="text" className="form-control" placeholder="Hotel License Number" />
+            <input type="text" className="form-control"
+             placeholder="Hotel License Number"
+             value={hRegNo}
+             onChange={(e) => setHRegNo(e.target.value)} />
 
           </div>
           <div className="mb-3 d-flex" >
             <input
               type="text"
               className="form-control"
-              placeholder="Hotel Address" style={{ marginRight: '80px', width: '100%' }}
+              placeholder="Hotel Address"
+              style={{ marginRight: '80px', width: '100%' }}
+              value={hAddress}
+              onChange={(e) => setHAddress(e.target.value)}
             />
 
 
@@ -157,10 +207,18 @@ export const HotelRegistration = () => {
             <input
               type="text"
               className="form-control"
-              placeholder="Pincode" style={{ marginRight: '40px' }}
+              placeholder="Pincode"
+              style={{ marginRight: '40px' }}
+              value={hpinCode}
+              onChange={(e) => setHPinCode(e.target.value)}
             />
-            <input type="text" className="form-control" placeholder="Latitude " style={{ marginRight: '40px' }} />
-            <input type="text" className="form-control" placeholder="Longitude " />
+            <input type="text" className="form-control" placeholder="Latitude " 
+            style={{ marginRight: '40px' }} 
+            value={hLat}
+             onChange={(e) => setHLat(e.target.value)}/>
+            <input type="text" className="form-control" placeholder="Longitude " 
+            value={hLong}
+            onChange={(e) => setHLong(e.target.value)}/>
           </div>
           <p className='d-flex justify-content-end'><span style={{ color: 'red', fontSize: '12px' }}>Use My Current Location
           </span></p>
@@ -207,6 +265,8 @@ export const HotelRegistration = () => {
               type="text"
               className="form-control"
               placeholder="Username" style={{ marginRight: '80px' }}
+              value={hUsername}
+              onChange={(e) => setHUsername(e.target.value)}
             />
 
           </div>
--- src/apis/Hotel_Services.js
+++ src/apis/Hotel_Services.js
@@ -48,3 +48,44 @@ export const getPoliceStationListRequest = async(stateID,distID,cityID)=>{
      });
      return res
 }
+
+export const submitHotelRegistration = async(fName,lName,mNumber,eMail,hName,hRegNo,hAddress,hpinCode,hLat,hLong,hUsername,stateID, distID,cityData,psData) => {
+    const res = await axios({
+        method:'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data: JSON.stringify({ 
+            hotelName: hName,
+            hotelRegNo: hRegNo,
+            userId: hUsername,
+            firstName: fName,
+            lastName: lName,
+            mobile: mNumber,
+            email: eMail,
+            address: hAddress,
+            pinCode: parseInt(hpinCode),
+            stateId: stateID,
+            distId: distID,
+            cityId: cityData,
+            stationCode: String(psData),
+            lat: parseFloat(hLat),
+            _long: parseFloat(hLong),
+            diviceIp: "fsdkfjksd"
+         }), 
+      
+        url:'https://hotelapi.ictsbm.com/api/Account/HotelRegistration' ,
+     });
+     if (!res.ok) {
+        //throw new Error(`${res}`);
+        console.log(res);
+        console.log("Status Code: ",res.code);
+        console.log("Status Status: ",res.status);
+        console.log("Status MEssage: ",res.message);
+      }
+      console.log(res);
+      console.log("Status Code: ",res.code);
+      console.log("Status Status: ",res.status);
+      console.log("Status MEssage: ",res.message);
+      return res.json();
+}
